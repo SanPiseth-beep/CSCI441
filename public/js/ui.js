93384568518ffa5ba2255b6f77612a2d6881dd34
SanPiseth-beep <81914277+SanPiseth-beep@users.noreply.github.com>
2024-10-27 00:35:33 -0500

    Refactor UI initialization in ui.js and update background images in landing.css

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,8 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Sidenav Initialization
   const menus = document.querySelector(".sidenav");
-  M.Sidenav.init(menus, { edge: "right" });
-  //Add Tasks
-  const forms = document.querySelector(".side-form");
-  M.Sidenav.init(forms, { edge: "left" });
+  M.Sidenav.init(menus, { edge: "left" });
 });
