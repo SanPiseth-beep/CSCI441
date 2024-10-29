dc67870aeaaaa85c2b2af6cc04f5728cfada9869
Angel4623 <Angel462312@gmail.com>
2024-10-29 14:05:55 -0600

    homepage- edit background color

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,3 +2,7 @@ exports.index = (req, res) => {
     res.render('index', {title: 'Homepage', message: 'Welcome to the homepage!'});
   };
   
+exports.homepage1 = (req, res) => {
+    res.render('../public/pages/homepage1');
+  };
+
