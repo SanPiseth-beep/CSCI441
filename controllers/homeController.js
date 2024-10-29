d5933317d65abe16c671271c68282a83beac9e11
Nilin Lay <laynilin@gmail.com>
2024-10-28 21:32:31 -0500

    finished renting page 1

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,4 +1,8 @@
 exports.index = (req, res) => {
     res.render('index', {title: 'Homepage', message: 'Welcome to the homepage!'});
   };
+
+exports.renting1 = (req, res) => {
+  res.render('../public/pages/renting1');
+};
   
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const homeController = require('../controllers/homeController');
 
 router.get('/', homeController.index);
 
+router.get('/renting1', homeController.renting1);
 
 module.exports = router;
